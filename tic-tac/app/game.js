b757522812ff19f4a631d9a4e6495cba9fd97e85
Rahdeg <walett95@gmail.com>
2024-04-28 13:12:45 +0100

    tic

---
+++ tic-tac/app/game.js
@@ -0,0 +1,90 @@
+"use client";
+
+// src/components/Game.js
+import React, { useState } from "react";
+import Board from "./Board";
+
+const Game = () => {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const handleClick = (i) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const squares = [...current.squares];
+
+    if (winner || squares[i]) {
+      return;
+    }
+
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory([...newHistory, { squares }]);
+    setStepNumber(newHistory.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  let status;
+  if (winner && winner !== "Tie") {
+    status = `Winner: ${winner}`;
+  } else if (winner === "Tie") {
+    status = "Tie";
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={handleClick} />
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+    if (squares.every((square) => square !== null)) {
+      return "Tie";
+    }
+  }
+
+  return null;
+}
---
+++ tic-tac/app/page.tsx
@@ -0,0 +1,12 @@
+
+import Image from "next/image";
+import Game from "./game"
+
+export default function Home() {
+
+  return (
+    <div className=" flex flex-col    ">
+      <Game />
+    </div>
+  );
+}
